ea93a8e81527a66134e56ae9d610d84d37b4933d
loser <285013854@qq.com>
2017-11-28 02:29:48 +0800

    热部署设置

--- src/main/ui/config/index.js
+++ src/main/ui/config/index.js
@@ -27,11 +27,13 @@ module.exports = {
   },
   dev: {
     env: require('./dev.env'),
-    port: process.env.PORT || 8080,
+    port: 3000,
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      '/api/**': 'http://localhost:8080'
+    },
     // CSS Sourcemaps off by default because relative paths are "buggy"
     // with this option, according to the CSS-Loader README
     // (https://github.com/webpack/css-loader#sourcemaps)
