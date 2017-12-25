f7d226f0950ea2706abb6e712db18516d54f11a2
Loser <285013854@qq.com>
2017-12-26 02:26:07 +0800

    整理并去除无用配置

--- src/main/java/team/oha/laboa/config/WebConfig.java
+++ src/main/java/team/oha/laboa/config/WebConfig.java
@@ -2,12 +2,19 @@ package team.oha.laboa.config;
 
 import com.google.common.base.Charsets;
 import org.springframework.lang.Nullable;
+import org.springframework.web.context.support.ServletContextResource;
 import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.filter.DelegatingFilterProxy;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 import org.springframework.web.util.IntrospectorCleanupListener;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.EnumSet;
 
 /**
@@ -44,6 +51,9 @@ public class WebConfig extends AbstractAnnotationConfigDispatcherServletInitiali
         registerIntrospectorCleanupListener(servletContext);
         registerCharacterEncodingFilter(servletContext);
         super.onStartup(servletContext);
+        initIndexHtml(servletContext);
+        registerPageServlet(servletContext);
+        registerStaticResourceServlet(servletContext);
     }
 
     public void  registerIntrospectorCleanupListener(ServletContext servletContext){
@@ -66,5 +76,34 @@ public class WebConfig extends AbstractAnnotationConfigDispatcherServletInitiali
     @Override
     protected String[] getServletMappings() {
         return new String[]{"/api/*"};
+    }
+
+
+    private static final String INDEX_LOCATION = "/index.html";
+    private static final String DEFAULT_SERVLET_NAME = "default";
+    private byte[] indexHtml;
+
+    protected void initIndexHtml(ServletContext servletContext){
+        ServletContextResource resource = new ServletContextResource(servletContext, INDEX_LOCATION);
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new InputStreamReader(resource.getInputStream(), "UTF-8"));
+            indexHtml = reader.readLine().getBytes("UTF-8");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected void registerPageServlet(ServletContext servletContext){
+        servletContext.addServlet("pageServlet", new HttpServlet() {
+            @Override
+            protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                resp.getOutputStream().write(indexHtml);
+            }
+        }).addMapping("/");
+    }
+
+    protected void registerStaticResourceServlet(ServletContext servletContext){
+        servletContext.getServletRegistrations().get(DEFAULT_SERVLET_NAME).addMapping("/static/*");
     }
 }
